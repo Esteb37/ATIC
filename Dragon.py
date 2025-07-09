a693d3dc5420fe3d8113ce76202221c5e13c5f5f
Esteb37 <esteban37padilla@gmail.com>
2025-07-09 18:42:54 +0200

    With boxes

--- Dragon.py
+++ Dragon.py
@@ -25,15 +25,21 @@ class Dragon:
         self.num_links = p.getNumJoints(self.robot_id)
         self.center_of_gravity = [0.0, 0.0, 0.0]
         self.link_positions = []
+        self.link_orientations = []
+        self.link_dimensions = []
         self.link_names = []
         self.total_mass = 0.0
 
-        self.get_com()
+        self._drawn_artists = []
 
-    def get_com(self):
+        self.update_kinematics()
+
+    def update_kinematics(self):
         self.total_mass = 0.0
         weighted_com = [0.0, 0.0, 0.0]
         self.link_positions = []
+        self.link_orientations = []
+        self.link_dimensions = []
         self.link_names = []
 
         for link_index in range(-1, self.num_links):
@@ -41,38 +47,48 @@ class Dragon:
             mass = dynamics[0]
 
             if link_index == -1:
-                com_pos, _ = p.getBasePositionAndOrientation(self.robot_id)
-                link_name = p.getBodyInfo(self.robot_id)[1].decode('utf-8')
+                pos, orn = p.getBasePositionAndOrientation(self.robot_id)
+                name = p.getBodyInfo(self.robot_id)[1].decode('utf-8')
+                shape_data = []
             else:
-                com_pos, _ = p.getLinkState(self.robot_id, link_index, computeForwardKinematics=True)[0:2]
-                link_name = p.getJointInfo(self.robot_id, link_index)[12].decode('utf-8')
+                pos, orn = p.getLinkState(self.robot_id, link_index, computeForwardKinematics=True)[0:2]
+                name = p.getJointInfo(self.robot_id, link_index)[12].decode('utf-8')
+                shape_data = p.getCollisionShapeData(self.robot_id, link_index)
 
-            if "origin" in link_name or "modular" in link_name:
-                continue
-
-            self.link_positions.append(com_pos)
-            self.link_names.append(link_name)
+            self.link_positions.append(pos)
+            self.link_orientations.append(orn)
+            self.link_names.append(name)
+            if shape_data and len(shape_data) > 0:
+                dims = shape_data[0][3]
+                self.link_dimensions.append([d for d in dims])
+            else:
+                self.link_dimensions.append([0.05, 0.05, 0.05])
 
             if mass > 0:
-                weighted_com = [w + mass * c for w, c in zip(weighted_com, com_pos)]
+                weighted_com = [w + mass * c for w, c in zip(weighted_com, pos)]
                 self.total_mass += mass
 
         if self.total_mass > 0:
             self.center_of_gravity = [w / self.total_mass for w in weighted_com]
 
-        return self.center_of_gravity
+    def set_joint_pos(self, name_or_id, value):
 
-    def set_joint_pos(self, name, value):
-        name = name.lower()
-        for i in range(self.num_links):
-            joint_name = p.getJointInfo(self.robot_id, i)[12].decode('utf-8').lower()
-            if name in joint_name:
-                p.setJointMotorControl2(self.robot_id, i, p.POSITION_CONTROL,
-                                        targetPosition=value,
-                                        force=1.0,
-                                        maxVelocity=1.0)
-                return
-        raise ValueError(f"Joint '{name}' not found.")
+        if isinstance(name_or_id, int):
+            p.setJointMotorControl2(self.robot_id, name_or_id, p.POSITION_CONTROL,
+                                    targetPosition=value,
+                                    force=1.0,
+                                    maxVelocity=5.0)
+        else:
+            name = name_or_id.lower()
+            for i in range(self.num_links):
+                joint_name = p.getJointInfo(self.robot_id, i)[12].decode('utf-8').lower()
+                if name in joint_name:
+                    p.setJointMotorControl2(self.robot_id, i, p.POSITION_CONTROL,
+                                            targetPosition=value,
+                                            force=1.0,
+                                            maxVelocity=5.0)
+                    return
+            raise ValueError(f"Joint '{name}' not found.")
 
     def hover(self):
         thrust_force = [0, 0, 9.82 * self.total_mass]  # Newtons upward to hover
@@ -85,9 +101,23 @@ class Dragon:
 
     ######################### SIMULATION #############################
 
+    def thrust(self, forces):
+        rotor_indices = [i for i in range(self.num_links) if "rotor" in p.getJointInfo(self.robot_id, i)[12].decode('utf-8').lower()]
+        if len(rotor_indices) != len(forces):
+            raise ValueError("Number of forces must match number of rotors.")
+
+        for i, force in zip(rotor_indices, forces):
+            p.applyExternalForce(self.robot_id, i, [0, 0, force], self.link_positions[i], p.WORLD_FRAME)
+
+    def lock_joints(self):
+        # Lock all joints to their current positions
+        for i in range(self.num_links):
+            self.set_joint_pos(i, p.getJointState(self.robot_id, i)[0])
+
+
     def step(self):
         p.stepSimulation()
-        self.get_com()
+        self.update_kinematics()
         sleep(1/240)
 
     def _get_color(self, link):
@@ -98,15 +128,28 @@ class Dragon:
             return "red"
         return "blue"
 
+    def draw_box(self, ax, pos, orn, dims, color):
+        rot_matrix = np.array(p.getMatrixFromQuaternion(orn)).reshape(3, 3)
+        l, w, h = dims
+        corners = np.array([
+            [-l/2, -w/2, -h/2], [ l/2, -w/2, -h/2], [ l/2,  w/2, -h/2], [-l/2,  w/2, -h/2],
+            [-l/2, -w/2,  h/2], [ l/2, -w/2,  h/2], [ l/2,  w/2,  h/2], [-l/2,  w/2,  h/2]
+        ])
+        rotated = (rot_matrix @ corners.T).T + np.array(pos)
+        faces = [
+            [rotated[i] for i in [0, 1, 2, 3]], [rotated[i] for i in [4, 5, 6, 7]],
+            [rotated[i] for i in [0, 1, 5, 4]], [rotated[i] for i in [2, 3, 7, 6]],
+            [rotated[i] for i in [1, 2, 6, 5]], [rotated[i] for i in [0, 3, 7, 4]]
+        ]
+        box = Poly3DCollection(faces, facecolors=color, linewidths=0.5, edgecolors='k', alpha=0.8)
+        ax.add_collection3d(box)
+        self._drawn_artists.append(box)
+
+
     def init_plot(self):
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111, projection='3d')
-
         self.scatter_cog = self.ax.scatter([], [], [], color='g', s=100, label='Center of Gravity')
-        self.scatter_links = [self.ax.scatter([], [], [], color=self._get_color(name), s=20)
-                              for name in self.link_names]
-
-
         self.plot_plane(self.ax)  # Add this call to plot the plane
         self.ax.set_xlim([-2, 2])
         self.ax.set_ylim([-2, 2])
@@ -115,16 +158,22 @@ class Dragon:
         self.ax.set_ylabel('Y')
         self.ax.set_zlabel('Z')
         self.ax.set_title('Dragon URDF Center of Mass Visualization')
-        self.ax.legend()
+        self.box_artists = []
 
     def update_plot(self, frame):
+        for artist in self._drawn_artists:
+            artist.remove()
+        self._drawn_artists.clear()
+
+        for pos, orn, dims, name in zip(self.link_positions, self.link_orientations, self.link_dimensions, self.link_names):
+
+            if name[0] == "G" or name[0] == "F":
+                color = self._get_color(name)
+                self.draw_box(self.ax, pos, orn, dims, color)
+
         cog = self.center_of_gravity
         self.scatter_cog._offsets3d = ([cog[0]], [cog[1]], [cog[2]])
 
-        for scatter, pos in zip(self.scatter_links, self.link_positions):
-            scatter._offsets3d = ([pos[0]], [pos[1]], [pos[2]])
-
-        return [self.scatter_cog] + self.scatter_links
 
     def plot_plane(self, ax, size=2.0, z=0.0, color='gray', alpha=0.3):
         # Define the corners of the plane square
--- simulation.py
+++ simulation.py
@@ -2,9 +2,20 @@ from Dragon import Dragon
 import threading
 
 def sim_loop(dragon):
+
+
+    dragon.set_joint_pos("joint3_pitch", 0)
+    dragon.set_joint_pos("joint2_pitch", 0)
+    dragon.set_joint_pos("joint1_pitch", 0)
+    dragon.set_joint_pos("joint3_yaw", 1.5)
+    dragon.set_joint_pos("joint2_yaw", 1.5)
+    dragon.set_joint_pos("joint1_yaw", 1.5)
+
+
+    thrust = 9.81 * dragon.total_mass / 8
     while True:
         dragon.step()
-        dragon.hover()
+        dragon.thrust([thrust] * 8)
 
 def main():
     dragon = Dragon()
